858454b676860c0b815ad0236e108a17d2c36d18
mingjiazhu <mzhu@ucsd.edu>
2022-04-23 21:53:57 -0700

    updated MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,15 +11,35 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        boolean validMarkdownLink = true;
         while(currentIndex < markdown.length()) {
+            //System.out.println(validMarkdownLink);
+            int imageMarker = markdown.indexOf("!",currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(closeParen-openParen==-1)
+            {
+                validMarkdownLink=false;
+                //System.out.println("link is empty");
+                currentIndex = closeParen + 1;
+                continue;
+            }
+            else if(openBracket==-1||openParen==-1||closeBracket==-1||closeParen==-1)
+            {
+                //System.out.println("no brackets or parantheses");
+                validMarkdownLink=false;
+                break;
+            }
+            if(validMarkdownLink==true)
+            {
+                //System.out.println("found");
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
+            validMarkdownLink=true;
         }
-
         return toReturn;
     }
 
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,26 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.junit.*;
 public class MarkdownParseTest {
 
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException {   
+        MarkdownParse m = new MarkdownParse();
+        List l = List.of("https://something.com", "some-thing.html");
+        Path p = Paths.get("test-file.md");
+        String st = Files.readString(p);
+        assertEquals(l, m.getLinks(st));
     }
 
 }
